1064c4e46e5ed637a22f1c3257ed50dd71740302
ohtomi <ohtomi.kenichi@gmail.com>
2022-01-16 23:04:57 +0900

    Remove hello api

--- api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
-}
