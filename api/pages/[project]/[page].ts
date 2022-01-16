2c26ae1e1f22f9620e988974e7925e933751fb4c
ohtomi <ohtomi.kenichi@gmail.com>
2022-01-16 23:58:40 +0900

    Enable CORS in serverless function

--- api/pages/[project]/[page].ts
+++ api/pages/[project]/[page].ts
@@ -19,6 +19,18 @@ const fetchPageText = async (endpoint: string) => {
 }
 
 export default async (req: VercelRequest, res: VercelResponse) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
   const { project, page } = req.query
   const endpoint = `https://scrapbox.io/api/pages/${project}/${page}/text`
   const apiResponse = await fetchPageText(endpoint)
